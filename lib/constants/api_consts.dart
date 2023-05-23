393bcfa43b8e2b6c3d5c73e0475f071062685cd2
KarthikAvinash <karthikavinash01@gmail.com>
2023-05-23 07:59:51 +0530

    final commit

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
